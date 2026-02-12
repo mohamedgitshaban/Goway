d3b8b098d7021387f05856b9996457c5a3cc641b
mohamed-shaban-dafa <mohamed.shabaan@dafagate.com>
2026-02-12 14:26:16 +0200

    Add stubs for various Laravel components
    - Created stubs for events, factories, feature tests, jobs, and listeners.
    - Added stubs for mailables, middleware, migrations, models, notifications, and observers.
    - Introduced stubs for policies, providers, requests, resource collections, and resources.
    - Implemented route provider and route stubs for API and web routes.
    - Added validation rule stubs and scaffold configuration for modules.
    - Included Vite configuration and module loader for asset management.
    - Updated seeder and unit test stubs for consistency.
    - Enhanced view stubs for module layouts and index pages.

---
+++ Modules/Trip/Database/Seeders/TripDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Trip\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TripDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
